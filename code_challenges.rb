c7001a1d171f52b20891664b0ef962553a1befa2
Ubuntu <ubuntu@ip-172-31-61-141.us-west-2.compute.internal>
2020-03-02 23:33:33 +0000

    turning in week 4

--- code_challenges.rb
+++ code_challenges.rb
@@ -2,28 +2,68 @@
 
 # --------------------1) Create a method that takes in an array and returns an array with only odd numbers sorted from least to greatest.
 
-
-
-
-
 fullArr1 = [4, 9, 0, '7', 8, true, 'hey', 7, 199, -9, false, 'hola']
 # Expected output: [-9, 7, 9, 199]
 fullArr2 = ['hello', 7, 23, -823, false, 78, nil, '67', 6, 'Number']
 # Expected output: [-823, 7, 23]
 
 
+def odd_least(arr)
+    arr.select! { |el| el.is_a? Numeric }
+    arr.select(&:odd?).sort
+end
+p odd_least(fullArr1)
+p odd_least(fullArr2)
+
+
 # --------------------2) Create a class called Bike that is initialized with a model, wheels, and a frame size. The default number of wheels is 2. Create a get_info method that returns a sentence with all the data from the bike object.
 
+class Bike
+    def initialize (model, wheels, framesize, bell, speedometer)
+        @model = model
+        @wheels = 2
+        @framesize = framesize
+        @bell = "silent"
+        @speedometer = 0
+    end
+    def bell_change()
+        if @bell == 'silent'
+            @bell = 'Kring Ting!'
+        else
+            @bell = “silent”
+        end
+    end
+    def get_info
+        ["The #{@model}" "has #{@wheels} wheels" "and a #{@framesize} frame"]
+    end
+    def pedal_hard()
+        if @speedometer == 0
+            @speedometer = 10
+        else
+            @speedometer = @speedometer + 10
+        end
+    end
+    def crazy_brake()
+        if @speedometer <= 0
+            @speedometer = 0
+        else
+            @speedometer = @speedometer - 10
+        end
+    end
+end
 
 
-
+cool_bike = Bike.new('Priority Classic Plus', '', 'medium', '', '')
+p cool_bike.get_info
 
 # Expected output example: 'The Trek bike has 2 wheels and a 168cm frame.'
 
 
 # -------------------3) Add a bell to the bike class and create a method that will ring the bell when the method is called.
 
-
+cooler_bike = Bike.new('Bell Test', '', 'small', '', '')
+cooler_bike.bell_change
+p cooler_bike
 
 
 
@@ -32,7 +72,8 @@ fullArr2 = ['hello', 7, 23, -823, false, 78, nil, '67', 6, 'Number']
 
 # -------------------4) Add a speedometer to the Bike class. The speed should be initialized at 0.
 
-
+coolest_bike = Bike.new('Speed Test', '', 'large', '', '')
+p coolest_bike
 
 
 
@@ -41,9 +82,11 @@ fullArr2 = ['hello', 7, 23, -823, false, 78, nil, '67', 6, 'Number']
 
 # -------------------5) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed. The brake method should decrease the speed. The bike cannot go negative speeds.
 
-
-
-
-
+cool_bike.pedal_hard
+cool_bike.pedal_hard
+cool_bike.pedal_hard
+p cool_bike
+cool_bike.crazy_brake
+p cool_bike
 # Expected output example: my_bike.pedal_faster 10 => 10
 # Expected output example: my_bike.brake 15 => 0
--- coding-challenges.js
+++ coding-challenges.js
@@ -1,60 +1,105 @@
-// ASSESSMENT 4: JavaScript Coding Practical Questions
+ // ASSESSMENT 4: JavaScript Coding Practical Questions
 
-// --------------------1) Create a function that takes in an array. Each time the function is run, remove the first item from the array and shuffle the remaining content. If the array is empty, return "The array is empty."
+ // --------------------1) Create a function that takes in an array. Each time the function is run, remove the first item from the array and shuffle the remaining content. If the array is empty, return "The array is empty."
+
+ var collections = ["purple", "blue", "green", "yellow", "pink"]
+ // Expected output example1 (can be a different order): ["yellow", "blue", "pink", "green"]
+ // Expected output example2 (can be a different order): ["blue", "green", "pink"]
+ // Expected output example3 (can be a different order): ["pink", "green"]
+
+ const shuffleRemove = (arr) => {
+     arr.shift()
+     if (arr.length === 0) {
+     let arrEmp = "The array is empty."
+     return arrEmp
+     }
+     else
+        
+         for(let i=0, total = arr.length; i< total; i++ ){
+         const random = Math.floor(Math.random() * (arr.length - 0)) + 0
+         arr.push(arr.splice(random, 1)[0])
+     }
+     return arr
+ }
+
+ console.log(shuffleRemove(collections))
+ console.log(shuffleRemove(collections))
+ console.log(shuffleRemove(collections))
+ console.log(shuffleRemove(collections))
+ console.log(shuffleRemove(collections))
+
+ // --------------------2) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
+ var cubeAndSum1 = [2, 3, 4]
+ // Expected output: 99 
+ var cubeAndSum2 = [0, 5, 10]
+ // Expected output: 1125
+
+ const sumArrCubed1 = cubeAndSum1.reduce((result,number) => result + (number ** 3), 0)
+ const sumArrCubed2 = cubeAndSum2.reduce((result,number) => result + (number ** 3), 0)
+
+ console.log(sumArrCubed1)
+ console.log(sumArrCubed2)
+
+ // --------------------3) Create a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.
+
+ var nums1 = [3, 56, 90, -8, 0, 23, 6]
+ // Expected output: [-8, 90]
+ var nums2 = [109, 5, 9, -59, 8, 24]
+ // Expected output: [-59, 109]
+
+ const arrMinMax = (arr) => {
+     let minMax = []
+     arr.sort((a,b) => a-b)
+     minMax.unshift(arr.pop())
+     minMax.unshift(arr.shift())
+     return minMax
+ }
+
+ console.log(arrMinMax(nums1))
+ console.log(arrMinMax(nums2))
+
+
+ // --------------------4) Create a function that takes in a string and returns a string with every other letter capitalized.
+ var testString1 = "albatross"
+ // Expected output: "aLbAtRoSs"
+ var testString2 = "jabberwocky"
+ // Expected output: "jAbBeRwOcKy"
+
+ const stickyCaps = (arr) => {
+     var caps = "";
+     for (i=0; i < arr.length; i++) {
+        caps += i % 2 !== 0 ? arr.charAt(i).toUpperCase() : arr.charAt(i);
+     }
+     return caps  
+ }
+
+
+ console.log(stickyCaps(testString1))
+ console.log(stickyCaps(testString2))
+
+
+ // --------------------5) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator.
+ var arr1 = [3, 7, 10, 5, 4, 3, 3]
+ var arr2 = [7, 8, 2, 3, 1, 5, 4]
+ // Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
 
 
 
+ const compArr = arr  => arr.reduce((a, b) => {
+     if (a.includes(b)) return a
+     return [...a, b]
+ }, [])
 
+ console.log(compArr(arr1.concat (arr2)))
 
-var collections = ["purple", "blue", "green", "yellow", "pink"]
-// Expected output example1 (can be a different order): ["yellow", "blue", "pink", "green"]
-// Expected output example2 (can be a different order): ["blue", "green", "pink"]
-// Expected output example3 (can be a different order): ["pink", "green"]
+ const sadArr = (arrA, arrB)  => {
+    // let comb = arrA.concat(arrB)  
+    let comb = [...new Set([...arrA, ...arrB])]
+    comb.reduce((a, b) => {
+    if (a.includes(b)) return a
+    return [...a, b]
+}, [])
+}
+console.log(sadArr(arr1, arr2))
 
-
-// --------------------2) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
-
-
-
-
-
-var cubeAndSum1 = [2, 3, 4]
-// Expected output: 99
-var cubeAndSum2 = [0, 5, 10]
-// Expected output: 1125
-
-
-
-// --------------------3) Create a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.
-
-
-
-
-
-var nums1 = [3, 56, 90, -8, 0, 23, 6]
-// Expected output: [-8, 90]
-var nums2 = [109, 5, 9, -59, 8, 24]
-// Expected output: [-59, 109]
-
-
-// --------------------4) Create a function that takes in a string and returns a string with every other letter capitalized.
-
-
-
-
-
-var testString1 = "albatross"
-// Expected output: "aLbAtRoSs"
-var testString2 = "jabberwocky"
-// Expected output: "jAbBeRwOcKy"
-
-
-// --------------------5) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator.
-
-
-
-
-
-var arr1 = [3, 7, 10, 5, 4, 3, 3]
-var arr2 = [7, 8, 2, 3, 1, 5, 4]
-// Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
+// I can solve this without cheating and without reduce but I just really wanted reduce to work 
